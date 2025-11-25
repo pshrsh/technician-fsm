bd5017abfacc98809d870037439a0aaed730a8ba
dvir laptop <dvirdayan810@gmail.com>
2025-11-25 16:25:17 +0200

    technitian

--- domain/Entities/Technician.cs
+++ domain/Entities/Technician.cs
@@ -1,6 +1,16 @@
 public static class Technician
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Specialty { get; set; }
+    public string BaseLocation { get; set; }
+    public string[] Skills { get; set; }
+    public string StartHours { get; set; }
+    public string EndHours { get; set; }
+    public task[] AssignedTasks { get; set; }
+    public Technician(string baseLocation, string[] skills, string startHours, string endHours)
+    {
+        BaseLocation = baseLocation;
+        Skills = skills;
+        StartHours = startHours;
+        EndHours = endHours;
+        AssignedTasks = new task[] { };
+    }
 }
